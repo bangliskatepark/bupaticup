584a132793641e836029fa45f6df904e307bdcc1
Epen Flow <53342177+epenflow@users.noreply.github.com>
2023-08-18 22:07:26 +0800

    update home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import styled from '@emotion/styled';
 import React from 'react';
 import { latarBelakang } from '../constants/ConstText';
-const HomeWrapper = styled.div`
+import poster from '../assets/poster.jpeg';
+interface IHomeWrapper {
+	img: string;
+}
+const HomeWrapper = styled.div<IHomeWrapper>`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -13,10 +17,10 @@ const HomeWrapper = styled.div`
 
 	h1 {
 		text-transform: capitalize;
-		color: red;
+		color: #ffffff;
 	}
-	span {
-		height: 50vh;
+	div:nth-of-type(1) {
+		height: 50%;
 		overflow-y: auto;
 		display: flex;
 		flex-direction: column;
@@ -27,15 +31,30 @@ const HomeWrapper = styled.div`
 			text-align: justify;
 			padding: 0.25rem;
 			font-size: 16px;
+			color: #ffffff;
 		}
 	}
-
-	div {
+	div:nth-of-type(2) {
+		position: absolute;
+		z-index: -2;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		span {
+			display: block;
+			position: relative;
+			background-image: url(${(props) => props.img});
+			background-size: cover;
+			background-repeat: no-repeat;
+			width: 100%;
+			height: 100%;
+		}
+	}
+	div:nth-of-type(3) {
 		background-color: red;
 		bottom: 0%;
 		margin-top: 0.5rem;
 		padding: 0.5rem 4rem 0.5rem 4rem;
-
 		border-radius: 10rem;
 		h1 {
 			color: white;
@@ -44,13 +63,15 @@ const HomeWrapper = styled.div`
 `;
 const Home: React.FC = () => {
 	return (
-		<HomeWrapper>
+		<HomeWrapper img={poster}>
 			<h1>Latar Belakang</h1>
-			<span>
+			<div>
 				<p>{latarBelakang.paragraphone}</p>
 				<p>{latarBelakang.paragraphtwo}</p>
-			</span>
-
+			</div>
+			<div>
+				<span />
+			</div>
 			<div>
 				<h1>bupati cup #2</h1>
 			</div>
